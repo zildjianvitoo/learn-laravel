79b6ef75f8657e1baf19df9e33b17d6f39129fdc
ZildjianVito <zildjianvito07@gmail.com>
2024-03-02 22:47:16 +0700

    learn: lil bit facotry faker

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
@@ -17,7 +18,13 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            "category_id" => 1,
+            "user_id" => 2,
+            "title" => fake()->text(10),
+            "slug" => "judul-" . Str::random(5),
+            "author" => Str::random(10),
+            "excerpt" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum, officiis.",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus illum, itaque mollitia animi a numquam voluptatibus nulla deleniti delectus odit? Officiis, atque aliquam temporibus natus asperiores ab cumque harum dolore!w",
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 
@@ -20,9 +23,11 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
+        User::factory()->count(5)->create();
+
         $this->call(
             [
-                UserSeeder::class,
+                // UserSeeder::class,
                 PostSeeder::class,
             ]
         );
